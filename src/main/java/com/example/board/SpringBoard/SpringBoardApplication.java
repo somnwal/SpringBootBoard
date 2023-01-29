ac19ab7394c034d32b4894782ebbdc691400c844
솜놜 <somnwal.kor@gmail.com>
2023-01-29 13:42:07 +0900

    Article 도메인추가

--- src/main/java/com/example/board/SpringBoard/SpringBoardApplication.java
+++ src/main/java/com/example/board/SpringBoard/SpringBoardApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.board.SpringBoard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringBoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringBoardApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/board/SpringBoard/SpringBoardApplicationTests.java
+++ src/test/java/com/example/board/SpringBoard/SpringBoardApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.board.SpringBoard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringBoardApplicationTests {
 
 	@Test
 	void contextLoads() {
